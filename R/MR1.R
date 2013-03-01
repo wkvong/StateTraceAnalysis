854ca7c0c36a1df0027105fd023589424f5c86bd
wkvong <waikeenvong@gmail.com>
2013-03-01 12:26:13 +1030

    Added cell2adj function in R, updated .gitignore

--- R/MR1.R
+++ R/MR1.R
@@ -20,7 +20,8 @@ adj2ineq <- function(adj) {
   j <- which(A != 0, arr.ind = TRUE)[, 2] # get column indices of non-zero elements
   s <- t(A)[t(A) != 0] # get values of non-zero elements, need to transpose to get correct order
 
-  Aineq <- matrix(0, length(i), nrow(adj)) # create zero matrix 
+  # initialize inequality matrix with zeros
+  Aineq <- matrix(0, nrow = length(i), ncol = nrow(adj)) 
 
   # fill in matrix with inequality coefficients
   for(k in 1:length(i)) {
@@ -32,14 +33,43 @@ adj2ineq <- function(adj) {
 }
 
 cell2adj <- function(nodes, E=list()) {
-  return(E)
+  # converts a partial order model in cell array form to an adjacency matrix suitable for monotonic regression
+  
+  if(!is.list(E))
+    E <- list(E) # probably won't work unless input is a set of lists, TODO: ask john about this
+
+  n <- length(nodes)
+  print(n)
+  
+  # initialize adjacency matrix with zeros
+  adj <- matrix(0, nrow = n, ncol = n)
+
+  # fill in adjacency matrix
+  if(length(E) != 0) {
+    for(i in 1:length(E)) {
+      if(length(E[[i]]) != 0) {
+        # TODO: ask john what is wrong
+        ## u <- choose(E[[i]], 2)
+        u <- matrix(E[[i]], nrow = 1) # convert u to matrix to use nrow
+        for(j in 1:nrow(u)) {
+          k1 <- which(nodes == u[j, 1])
+          k2 <- which(nodes == u[j, 2])
+          adj[k1, k2] = 1;
+        }
+      }
+    }
+  }
+  
+  return(adj)
 }
 
 # for testing
 A <- matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2, byrow = TRUE)
 
-A <- matrix(c(1, 2, 0, 0, 2, 1, 1, 2, 3), nrow = 3, byrow = TRUE)
+A <- matrix(c(1, 1, 0, 0, 1, 1, 1, 1, 1), nrow = 3, byrow = TRUE)
 
-B <- adj2ineq(A)
+E <- adj2cell(A)
 
-C <- cell2adj(c(1, 2))
+nodes <- c(1, 2, 3)
+
+C <- cell2adj(nodes, E)
