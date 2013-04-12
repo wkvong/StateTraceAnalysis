cac7a1a1d9955432c7213f262a171946b64ff843
wkvong <waikeenvong@gmail.com>
2013-04-12 16:58:45 +0930

    Cleaning up variable names, added more TODOs to cleaning up code

--- R/MR1.R
+++ R/MR1.R
@@ -1,10 +1,12 @@
 ## TODO: use R coding style
+## TODO: replace output variables with better names + other variables
+## TODO: function documentation
+## TODO: move tests to another file?
 
 library(limSolve)
 library(expm)
 library(R.matlab)
 
-## TODO: function documentation
 staCMR <- function(data, E = list()) {
   tol <- 10e-5
 
@@ -26,7 +28,7 @@ staCMR <- function(data, E = list()) {
 
   x <- output[[1]]
   f <- output[[2]]
-  Eprime <- output[[3]]
+  e.prime <- output[[3]]
   
   f[f < tol] <- 0
 
@@ -35,7 +37,7 @@ staCMR <- function(data, E = list()) {
     f <- f[1]
   }
 
-  return(list(x, f, Eprime))
+  return(list(x, f, e.prime))
 }
 
 CMR <- function(y, E) {
@@ -45,10 +47,10 @@ CMR <- function(y, E) {
   ## E is a cell array of the starting partial order model: E for Edges
   ## for example, if we want x3 <= x2 <= x1 and x4 <= x3 then
   ## E = {[3 2 1] [4 3]}, and so on.
-  ## Fbar is the fit of the starting model -- 'Inf' is a good start
+  ## f.bar is the fit of the starting model -- 'Inf' is a good start
   ## returns:
   ## xystar = best fitting values
-  ## Fbar = weighted least squares fit
+  ## f.bar = weighted least squares fit
   ## Estar = final partial order model 
   ## exitflag = vector of exit flags for fits for each variable
 
@@ -65,39 +67,32 @@ CMR <- function(y, E) {
 
   L[[1]][[2]] <- -Inf
 
-  Fbar <- Inf
-  eBar <- L[[1]][[1]]
+  f.bar <- Inf
+  e.bar <- L[[1]][[1]]
 
   while(length(L) > 0) {
-    Eprime <- L[[1]][[1]]
-    Ffloor <- L[[1]][[2]]
+    e.prime <- L[[1]][[1]]
+    f.floor <- L[[1]][[2]]
 
-    ## print(Eprime)
-    ## print(Ffloor)
-    
     L[[1]] <- NULL ## remove this element from the list
 
-    ## print(Ffloor)
-    ## print(Fbar)
-    
-    if (Ffloor < Fbar) {
-      output <- staMR(y, Eprime)
-      xPrime <- output[[1]]
+    if (f.floor < f.bar) {
+      output <- staMR(y, e.prime)
+      x.prime <- output[[1]]
       fits <- output[[2]]
 
-      Ffit <- sum(fits)
+      f.fit <- sum(fits)
 
-      if (Ffit < Fbar) {
-        feas <- feasible(xPrime)
+      if (f.fit < f.bar) {
+        feas <- feasible(x.prime)
         flag <- feas[[1]]
         idx <- feas[[2]]
 
         if (flag) {
-          # TODO: check later
-          Fbar <- Ffit
-          Ffits <- fits
-          xBar <- xPrime
-          eBar <- Eprime
+          f.bar <- f.fit
+          f.fits <- fits
+          x.bar <- x.prime
+          e.bar <- e.prime
         }
         else {
           ## create two branches
@@ -105,29 +100,29 @@ CMR <- function(y, E) {
           L[[end]] <- list()
           L[[end + 1]] <- list()
 
-          L[[end]][[1]] <- Eprime
+          L[[end]][[1]] <- e.prime
           L[[end]][[1]][idx[1], idx[2]] <- 1 ## (i,j) branch
-          L[[end]][[2]] <- Ffit
-          L[[end+1]][[1]] <- Eprime
+          L[[end]][[2]] <- f.fit
+          L[[end+1]][[1]] <- e.prime
           L[[end+1]][[1]][idx[2], idx[1]] <- 1 ## (j,i) branch
-          L[[end+1]][[2]] <- Ffit
+          L[[end+1]][[2]] <- f.fit
         }
       }
     }
   }
 
-  xStar <- xBar
-  eStar <- eBar
+  x.star <- x.bar
+  e.star <- e.bar
 
   ## TODO: return exitflag
-  return(list(xStar, Ffits, eStar))
+  return(list(x.star, f.fits, e.star))
 }
 
-## TODO: test this function
+## TODO: rename xx variable?
 feasible <- function(xx) {
   flag <- 1
   idx <- vector()
-  zero <- 1e-10
+  tol <- 1e-10
   x <- matrix(0, length(xx[[1]]), length(xx))
   
   for (i in 1:length(xx)) {
@@ -138,12 +133,13 @@ feasible <- function(xx) {
   u <- t(u) ## transpose u to be consistent with matlab
   d <- x[u[, 1], ] - x[u[, 2], ]
 
-  k <- which(abs(d) <= zero) 
+  k <- which(abs(d) <= tol) 
   d[k] <- 0 ## set zero to any tiny differences
   d <- sign(d) ## convert to signs
-  
+
+  ## TODO: check if this can be better written
   if(is.vector(d)) {
-    s <- abs(d) - abs(d) ## TODO: check if ok
+    s <- abs(d) - abs(d)
   }
   else {
     s <- rowSums(abs(d)) - abs(rowSums(d)) ## s > 0 if signs of difference are not equal
@@ -173,7 +169,6 @@ staMR <- function(data, E = list()) {
   }
 
   if (!is.list(y[[1]])) {
-
     y = staSTATS(y);
   }
   
@@ -182,9 +177,9 @@ staMR <- function(data, E = list()) {
   ## TODO: exitflag information
 
   for(i in 1:length(y)) {
-    yy <- y[[i]]
+    yy <- y[[i]] ## TODO: rename?
 
-    output <- MR1(yy$means, yy$weights, E)
+    output <- MR(yy$means, yy$weights, E)
     
     x[[i]] <- output$X
     f[i] <- output$solutionNorm
@@ -194,9 +189,9 @@ staMR <- function(data, E = list()) {
   ## round numbers close to tolerance level
   f[f < tol] <- 0
 
-  ## if (!is.list(data)) {
-  ##   x <- x[[1]] ## TODO: what does this do?
-  ## }
+  if (!is.list(data)) {
+    x <- x[[1]] ## TODO: check what this does?
+  }
 
   ## TODO: return exitflag as well
   return(list(x, f))
@@ -217,12 +212,14 @@ staSTATS <- function(data) {
     y <- list(y)
   }
 
+  ## TODO: rename output variable
   output <- rep(list(list()), length(y))
 
-  for(idata in 1:length(y)) {
-    yy <- y[[idata]]
+  for(i in 1:length(y)) {
+    yy <- y[[i]] ## TODO: rename yy
     yy <- yy[complete.cases(yy), ] ## delete rows with NAs
 
+    ## TODO: change how list is named?
     out <- list()
     out.names <- c("means", "n", "cov", "weights", "lm")
 
@@ -246,7 +243,7 @@ staSTATS <- function(data) {
     names(out) <- out.names
 
     ## add to output
-    output[[idata]] <- out
+    output[[i]] <- out
   }
 
   if (!is.list(output)) {
@@ -256,7 +253,8 @@ staSTATS <- function(data) {
   return(output)
 }
 
-MR1 <- function(y, w = diag(length(y)), E = matrix(0, length(y), length(y))) {
+## TODO: fix function documentation
+MR <- function(y, w = diag(length(y)), E = matrix(0, length(y), length(y))) {
   ## Uses lsqlin to solve standard monotonic regression problems
   ## MR conducts monotonic regression on each column separately
   ## y is a vector of values - the data
@@ -288,7 +286,7 @@ MR1 <- function(y, w = diag(length(y)), E = matrix(0, length(y), length(y))) {
     b <- matrix(0, nrow(A), 1)
   }
   else {
-    A <- NULL
+    A <- NULL ## not needed
     b <- NULL
   }
 
@@ -302,11 +300,13 @@ MR1 <- function(y, w = diag(length(y)), E = matrix(0, length(y), length(y))) {
 
   d <- C %*% y
 
-  L <- lsei(A = C, B = d, G = A, H = b, type=2, verbose = TRUE)
+  ## run weighted least squares regression with inequality constraints
+  L <- lsei(A = C, B = d, G = A, H = b, type=2, verbose = TRUE) 
 
   return(L)
 }
 
+## TODO: fix function documentation
 adj2cell <- function(adj) {
   ## converts adjacency matrix to cell array
   
@@ -319,6 +319,7 @@ adj2cell <- function(adj) {
   for(i in 1:length(row)) {
     E[[i]] <- c(row[i], column[i])
   }
+
   return(E)
 }
 
@@ -374,7 +375,7 @@ cell2adj <- function(nodes, E=list()) {
 
 ## w <- diag(3)
 
-## L <- MR1(y=c(2, 4, 8), w, E=A)
+## L <- MR(y=c(2, 4, 8), w, E=A)
 
 ## ## read in data and convert to a list
 ## x <- matrix(scan('../data/x.dat'), ncol = 5, byrow = TRUE)
@@ -388,4 +389,4 @@ cell2adj <- function(nodes, E=list()) {
 
 cmrData <- readMat('../data/nakabayashi.mat')
 cmrData <- cmrData$data
-staCMR(cmrData[1])
+staCMR(cmrData)
